8928cecde5a28ddbe927ff1fa74c5798824b4dfd
Mickael Cerisier <miccerisier@gmail.com>
2013-03-04 14:54:46 +0800

    Add Singleton object

--- Database.cpp
+++ Database.cpp
@@ -14,11 +14,16 @@ bool		Database::init(std::string const &path)
 
 	ret = this->open(path);
 	if (ret == false) return false;
-	ret = this->oneStepRequest("CREATE TABLE IF NOT EXISTS roads (id INTEGER PRIMARY KEY, name TEXT)");
+	ret = this->oneStepRequest("CREATE TABLE IF NOT EXISTS roads (id INTEGER PRIMARY KEY, name TEXT, speed INTEGER)");
 	if (ret == false) return false;
 	ret = this->oneStepRequest("CREATE TABLE IF NOT EXISTS nodes (id INTEGER PRIMARY KEY, x REAL, y REAL)");
 	if (ret == false) return false;
-	ret = this->oneStepRequest("CREATE TABLE IF NOT EXISTS links (id INTEGER PRIMARY KEY, idNode1 INTEGER, idNode2 INTEGER, idRoad INTEGER)");
+	ret = this->oneStepRequest("CREATE TABLE IF NOT EXISTS links (id INTEGER PRIMARY KEY, idNode1 INTEGER, idNode2 INTEGER, idRoad INTEGER, oneway INTEGER)");
 	if (ret == false) return false;
 	return ret;
+}
+
+void		Database::addRoad(Road &road)
+{
+
 }
