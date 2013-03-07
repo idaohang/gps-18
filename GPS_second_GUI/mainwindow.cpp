7e32fb71b734cefe340a05f13506aa12ee946820
castel_a <gianni.castel@gmail.com>
2013-03-08 04:07:17 +0800

    add background

--- GPS_second_GUI/mainwindow.cpp
+++ GPS_second_GUI/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "Converter.hpp"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -276,6 +277,23 @@ void MainWindow::parcourir()
         if (Database::get().init(sFileName.toStdString()))
         {
             Database::get().load();
+            // load background image
+
+            if (Database::get().images.size() > 0)
+            {
+                std::string img;
+                QImage              *mapImg;
+                img = "../app1_Editor/map/";
+                img += (*Database::get().images.rbegin()).second->path;
+                mapImg = new QImage(img.c_str());
+                this->scene->setBackgroundBrush(QBrush(*mapImg));
+                int mapWidth = mapImg->width();
+                int mapHeight = mapImg->height();
+                this->scene->setSceneRect(0.0, 0.0, mapWidth, mapHeight);
+                QString *tmp = new QString(Converter::toString(mapWidth).c_str());
+                tmp = new QString(Converter::toString(mapHeight).c_str());
+            }
+            //////////////////////////
 
             for (auto it = Database::get().nodes.begin(); it != Database::get().nodes.end(); ++it)
             {
